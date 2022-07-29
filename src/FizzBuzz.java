747c1511cd1357c110c2bf9c5365fce7859786d4
masakiyamada <109943822+MasakiYamada119@users.noreply.github.com>
2022-07-29 16:09:53 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,4 +1,19 @@
 
 public class FizzBuzz {
-
+	public static void main(String[] args) {
+	   int b=3;
+	   int c=5;
+	   int d=15;
+	   for (int a=1; a<=100; a++) {
+		   if (a%b==0) {
+			   System.out.println("Fizz");
+		   }else if (a%c==0) {
+			   System.out.println("Buzz");
+		   }else if (a%d==0) {
+			   System.out.println("FizzBuzz");
+		   }else {
+			   System.out.println(a);
+		   }
+	   }
+	}
 }
