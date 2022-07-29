29c34f634d3bf69c0170d054f0a5a3f71bd5545c
masakiyamada <109943822+MasakiYamada119@users.noreply.github.com>
2022-07-29 16:53:47 +0900

    再提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,12 +5,12 @@ public class FizzBuzz {
 	   int c=5;
 	   int d=15;
 	   for (int a=1; a<=100; a++) {
-		   if (a%b==0) {
+		   if (a%d==0) {
+			   System.out.println("FizzBuzz");
+		   }else if (a%b==0) {
 			   System.out.println("Fizz");
 		   }else if (a%c==0) {
 			   System.out.println("Buzz");
-		   }else if (a%d==0) {
-			   System.out.println("FizzBuzz");
 		   }else {
 			   System.out.println(a);
 		   }
